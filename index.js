4eaa5666387aeab8b07518147eeaa6c5dade8a3a
jango <44318485+thupten1994@users.noreply.github.com>
2024-11-22 18:44:13 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const Strategy = require("passport-local").Strategy
 const methodoverride = require("method-override")
 const app = express()
 
-
-const users = []
 app.use(express.json())
 app.set("view engine", "ejs")
 
@@ -26,98 +24,98 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use(methodoverride("_method"))
-
+const users= []
 
 app.get("/",checkAuthenticate, (req,res)=>{
 
-       res.render("index", { name : req.user.name})
+    res.render("h", { name : req.user.name, id : req.user.id})
 })
-
-
 
 app.get("/login",notcheckAuthenticate,(req,res)=>{
 
-    res.render("login")
+ res.render("l")
 })
+
+
+
 app.get("/register",notcheckAuthenticate,(req,res)=>{
-    res.render("register")
+ res.render("r")
 })
 
 
 app.post("/login",notcheckAuthenticate,passport.authenticate("local",{
-    successRedirect : "/",
-    failureRedirect :"login",
-    failureFlash: true
+ successRedirect : "/",
+ failureRedirect :"/login",
+ failureFlash: true
 }))
 
 
-app.post("/register",notcheckAuthenticate,  (req,res)=>{
+app.post("/register",notcheckAuthenticate,  async(req,res)=>{
 
-    try{
-        users.push({
-            id: Date.now(),
-            name : req.body.name,
-            email : req.body.email,
-            password : req.body.password}   
-       )
-       console.log(users)
-       res.render("login")
-    }
-    catch(e){
-        res.render("register")
-    }
+ try{
+       users.push ({
+        id: Date.now(),
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password
+      })
+      res.render("l")
+ }
+ catch(e){
+     res.render("r")
+ }
 
 })
 
 app.delete("/logout",(req,res)=>{
-    req.logout(function(err) {
-        if (err) {
-            return next(err);  // Handle the error if any
-        }
-        res.redirect('/login');  // Redirect after successful logout
-    });
+ req.logout(function(err) {
+     if (err) {
+         return next(err);  // Handle the error if any
+     }
+     res.redirect('/');  // Redirect after successful logout
+ });
 })
 passport.use( 
-    new Strategy({ 
-        usernameField : "email",
-        passwordField : "password"
-    },
-    (email,password,done)=>{
+ new Strategy({ 
+     usernameField : "email",
+     passwordField : "password"
+ },
+ (email,password,done)=>{
 
-         const user = users.find(u=>u.email===email)
-         if(!user) return done(null,false, {message : "invalid emailId"})
+      const user = users.find(u=>u.email===email)
+      if(!user) return done(null,false, {message : "invalid emailId"})
 
-        if(user.password!==password){
-            return done(null,false,{message :"invalid password"})
-        }
-        return done(null,user)
-    }))
+     if(user.password!==password){
+         return done(null,false,{message :"invalid password"})
+     }
+     return done(null,user)
+ }))
 passport.serializeUser((user,done)=>{
 
-    return done(null,user.id)
+ return done(null,user.id)
 })
 
 passport.deserializeUser((id,done)=>{
-              
-    const u = users.find(u=>u.id===id)
-     return done(null,u)
+           
+ const u = users.find(u=>u.id===id)
+  return done(null,u)
 })
+
 
 
 function checkAuthenticate(req,res,next){
 
-    if(req.isAuthenticated()){
-        return next()
-    }
-    return  res.redirect("/login")
-    
+ if(req.isAuthenticated()){
+     return next()
+ }
+ return  res.redirect("/login")
+ 
 }
 function notcheckAuthenticate(req,res,next){
 
-    if(req.isAuthenticated()){
-        return res.redirect("/")
-    }
-     next()
+ if(req.isAuthenticated()){
+     return res.redirect("/")
+ }
+  next()
 }
-
 app.listen(9000)
